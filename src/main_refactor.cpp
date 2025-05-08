5c8bd737e0bacaf0a757d404aca608bbb653124c
Kim Lasse Massesson <kima4@elev.tec.dk>
2025-05-08 18:28:12 +0200

    main_med_doxy

--- src/main_refactor.cpp
+++ src/main_refactor.cpp
@@ -1,93 +1,109 @@
-#include <WiFi.h>              // WiFi-funktioner for ESP32
-#include <OneWire.h>           // Bibliotek til at kommunikere med DS18B20
-#include <DallasTemperature.h> // Bibliotek, der bruges specifikt til at læse temperaturdata fra DS18B20-sensor
-#include <WiFiManager.h>       // Bibliotek der håndterer WiFi-forbindelse og konfigurationsportal til netværksvalg
-#include <SPIFFS.h>            // SPI Flash File System – bruges til at læse og skrive filer i flash-hukommelsen (fx CSV)
-#include <ESPAsyncWebServer.h> // Asynkront webserver/websocket-bibliotek til ESP32
-#include <AsyncTCP.h>          // TCP-understøttelse til asynkrone operationer – kræves af ESPAsyncWebServer
-#include "Arduino.h"
-#include "config.h"
-#include "csv/csv_handler.h"
-#include "wifi/wifi_handler.h"
-#include "time/time_handler.h"
-#include "reset/reset_handler.h"
-#include "route/route_handler.h"
-#include "sensor/sensor_handler.h"
-#include "websocket/websocket_handler.h"
-
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
-AsyncWebServer server(80);
-AsyncWebSocket ws("/ws");
-
-const char *filename = DATA_FILENAME;
-
-unsigned long previousMillis = 0;
-unsigned long interval = 30000;
-
-unsigned long buttonPressStart = 0;
-bool resetInitiated = false;
-
-char timeString[64];
-
-bool isReset = false;
-
 /**
- * @brief Setup-funktion.
+ * @file main.cpp
+ * @brief Hovedfil til ESP32 temperaturlogger med WebSocket, SPIFFS og WiFiManager.
  */
-void setup()
-{
-  Serial.begin(115200);
-  pinMode(RESET_BUTTON_PIN, INPUT_PULLUP);
 
-  sensors.begin();
-
-  initWiFi();
-  initTime("CET-1CEST,M3.5.0,M10.5.0/3");
-
-  if (!SPIFFS.begin(true))
-  {
-    Serial.println("SPIFFS initialisering fejlede");
-    return;
-  }
-
-  // Hent og formatér tid
-  getFormattedTime(timeString);
-
-  // Log temperatur og tid til CSV
-  logTemperatureToCSV(sensors, timeString);
-
-  // Webserver routes (Async-style)
-  // Root-side (forside)
-  setupRoutes();
-
-  // Tilføj WebSocket handler
-  ws.onEvent(onWebSocketEvent);
-  server.addHandler(&ws);
-
-  server.begin();
-}
-
-/**
- * @brief Loop-funktion. Logger temperatur, og overvåger WiFi og reset-knap.
- */
-void loop()
-{
-  static unsigned long lastLogTime = 0;
-  unsigned long currentMillis = millis();
-
-  checkResetButton(); // Kaldes hele tiden
-
-  if ((WiFi.status() != WL_CONNECTED) && (currentMillis - previousMillis >= interval))
-  {
-    WiFi.disconnect();
-    WiFi.reconnect();
-    previousMillis = currentMillis;
-  }
-
-  if (currentMillis - lastLogTime >= 300000)
-  { // 5 minutter
-    printLog();
-    lastLogTime = currentMillis;
-  }
-}
+ #include <WiFi.h>              ///< WiFi-funktioner for ESP32
+ #include <OneWire.h>           ///< Bibliotek til at kommunikere med DS18B20
+ #include <DallasTemperature.h> ///< Bibliotek til temperaturdata fra DS18B20-sensor
+ #include <WiFiManager.h>       ///< WiFi-konfiguration og portal
+ #include <SPIFFS.h>            ///< SPI Flash File System til filhåndtering
+ #include <ESPAsyncWebServer.h> ///< Asynkron webserver og WebSocket-support
+ #include <AsyncTCP.h>          ///< TCP-support til async-server
+ 
+ #include "Arduino.h"
+ #include "config.h"
+ #include "csv/csv_handler.h"
+ #include "wifi/wifi_handler.h"
+ #include "time/time_handler.h"
+ #include "reset/reset_handler.h"
+ #include "route/route_handler.h"
+ #include "sensor/sensor_handler.h"
+ #include "websocket/websocket_handler.h"
+ 
+ /// OneWire-bus til DS18B20
+ OneWire oneWire(ONE_WIRE_BUS);
+ 
+ /// Temperatur-sensor
+ DallasTemperature sensors(&oneWire);
+ 
+ /// Asynkron webserver på port 80
+ AsyncWebServer server(80);
+ 
+ /// WebSocket objekt på endpoint "/ws"
+ AsyncWebSocket ws("/ws");
+ 
+ /// Filnavn til CSV-log
+ const char *filename = DATA_FILENAME;
+ 
+ /// Tid siden sidste WiFi-reconnect
+ unsigned long previousMillis = 0;
+ 
+ /// Interval mellem WiFi-reconnects (ms)
+ unsigned long interval = 30000;
+ 
+ /// Starttidspunkt for reset-knap
+ unsigned long buttonPressStart = 0;
+ 
+ /// Flag for igangværende reset
+ bool resetInitiated = false;
+ 
+ /// Formatér tid som tekst
+ char timeString[64];
+ 
+ /// Flag for reset-status
+ bool isReset = false;
+ 
+ /**
+  * @brief Initialiserer systemet, WiFi, SPIFFS, sensorer og WebSocket.
+  */
+ void setup()
+ {
+   Serial.begin(115200);
+   pinMode(RESET_BUTTON_PIN, INPUT_PULLUP);
+ 
+   sensors.begin();
+   initWiFi();
+   initTime("CET-1CEST,M3.5.0,M10.5.0/3");
+ 
+   if (!SPIFFS.begin(true))
+   {
+     Serial.println("SPIFFS initialisering fejlede");
+     return;
+   }
+ 
+   getFormattedTime(timeString);              ///< Hent og formatér tid
+   logTemperatureToCSV(sensors, timeString);  ///< Log temperatur til CSV
+ 
+   setupRoutes(); ///< Initialiser webserver routes
+ 
+   ws.onEvent(onWebSocketEvent);  ///< WebSocket event-handler
+   server.addHandler(&ws);        ///< Tilføj WebSocket til server
+ 
+   server.begin(); ///< Start webserver
+ }
+ 
+ /**
+  * @brief Hovedloop. Logger temperaturperiodisk og håndterer WiFi og reset-knap.
+  */
+ void loop()
+ {
+   static unsigned long lastLogTime = 0;
+   unsigned long currentMillis = millis();
+ 
+   checkResetButton(); ///< Overvåg reset-knap
+ 
+   if ((WiFi.status() != WL_CONNECTED) && (currentMillis - previousMillis >= interval))
+   {
+     WiFi.disconnect();
+     WiFi.reconnect();
+     previousMillis = currentMillis;
+   }
+ 
+   if (currentMillis - lastLogTime >= 300000) // Hvert 5. minut
+   {
+     printLog(); ///< Logger temperatur og sender til klienter
+     lastLogTime = currentMillis;
+   }
+ }
+ 
